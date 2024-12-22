f7fef6345a6a46bcd77ae9eee3d8ae639887bdf9
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-12-22 12:39:43 +0530

    terraform count

---
+++ count/variables.tf
@@ -0,0 +1,18 @@
+# variable "instance_names" {
+#     type = list(string)
+#     default = ["mysql", "backend", "frontend"]
+# }
+
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags"{
+    type = map 
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
