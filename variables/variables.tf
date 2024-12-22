f6abc8df286faca5695e5e664c3f2a89cff26751
venkatabhinav54 <venkatabhinav54@gmail.com>
2024-12-22 11:25:27 +0530

    variables

---
+++ variables/variables.tf
@@ -0,0 +1,52 @@
+variable "ami_id" {
+    type      = string 
+    default   = "ami-09c813fb71547fc4f"
+    description = "This is the AMI ID of devops-practice"
+    
+}
+
+variable "instance_type" {
+    type = string
+    default  = "t3.micro"
+}
+
+
+variable "tags" {
+    type     = map #optional
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "DEV"
+        Terraform = "true"
+    }
+
+}
+
+variable "sg_name" {
+    default = "allow_sshh"
+}
+
+variable "sg_description" {
+    default = "Allow port number 22 for SSH access"
+}
+
+variable "from_port" {
+    default = 22
+    type = number
+}
+
+variable "to_port" {
+    default = 22
+    type = number
+}
+
+variable "protocal" {
+    default = "tcp"
+}
+
+
+variable "ingress_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
